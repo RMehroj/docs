10b35c09ab88e4c9480297dcc13a1cbd5070e813
RMehroj <bekmirzayevmehroj@gmail.com>
2024-04-18 11:17:36 +0500

    finshed update

--- api/v1/task/views.py
+++ api/v1/task/views.py
@@ -3,9 +3,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
-from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
-from django.contrib.auth.models import User
 from rest_framework import generics, filters
 
 from . import models
